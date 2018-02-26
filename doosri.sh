d640a2b6a5a08a6466c278a07988c006afea12e4
Sumitjh26997 <sumit.hotchandnai@gmail.com>
2018-02-26 11:50:52 +0530

    Initial Coimmit

---
+++ doosri.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+naam=jumma
+number=9211420420
+
+echo $naam
+echo $number
---
+++ pehli.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Pankaj : Are you tired ?"
+echo "Girl : Why ?"
+echo "Pankaj : Because you've been running around in my mind all day ."
