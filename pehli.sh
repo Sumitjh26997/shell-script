1cd1f6a4954f333ecbdd21b314436ca33c5f8743
Shikhar Bhatt <shikhar.bhatt011@gmail.com>
2019-01-24 19:59:25 +0530

    Changed name

--- pehli.sh
+++ pehli.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Pankaj : Are you tired ?"
+echo "Roshan : Are you tired ?"
 echo "Girl : Why ?"
-echo "Pankaj : Because you've been running around in my mind all day ."
+echo "Roshan : Because you've been running around in my mind all day ."
